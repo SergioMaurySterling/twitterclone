22c3c0cb22ad724400cee99aa04f943ade3148e0
Sergio Maury Sterling <psergio060897@gmail.com>
2021-09-09 08:45:40 -0500

    cambios importantes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ export const SearchBar = () => {
     return (
         <React.Fragment>
             <div className='search-container'>
-                <h5>Hey! SaerchBar here</h5>
+
             </div>
         </React.Fragment>
     )
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -6,7 +6,7 @@ export const Timeline = () => {
     return (
         <React.Fragment>
             <div className='timeline-container'>
-                <h5>Hey! Timeline here</h5>
+
             </div>
         </React.Fragment>
     )
